7f09155ffa1309258d06602dbfab3643a307658d
rhendz <patelankush.2012@gmail.com>
2019-12-21 17:48:55 -0600

    init

---
+++ random_sudoku_generator.py
@@ -0,0 +1,19 @@
+# Author: Ankush Patel
+# Contact: ankushp.com
+
+# Auxiliary sudoku program used to generate
+# random sudoku boards - this is meant to be used
+# in conjuction with 'sudoku.py'
+
+# The 'Sudoku' class implements a classic 9x9 sudoku board
+class Suduko:
+    def __init__(self, magic=20):
+        # Magic is the number of unsolved board spots
+        self.magic = magic
+
+def main():
+    print("This module is functional!")
+
+# This module is standalone
+if __name__ == "__main__":
+    main()
---
+++ sudoku.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.ndarray(shape=(2,2), dtype=float, order='F')
+print(x)
